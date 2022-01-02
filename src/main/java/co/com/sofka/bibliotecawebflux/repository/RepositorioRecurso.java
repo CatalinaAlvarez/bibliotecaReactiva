f554aefc6a39c1719471ca06b02100b1c03a84a1
CatalinaAlvarez <catalina.alvarez051@gmail.com>
2022-01-01 21:27:24 -0500

    Se añaden router, useCase y test para prestar, devolver y actualizar

--- src/main/java/co/com/sofka/bibliotecawebflux/repository/RepositorioRecurso.java
+++ src/main/java/co/com/sofka/bibliotecawebflux/repository/RepositorioRecurso.java
@@ -2,6 +2,8 @@ package co.com.sofka.bibliotecawebflux.repository;
 
 import co.com.sofka.bibliotecawebflux.collections.Recurso;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 
 public interface RepositorioRecurso extends ReactiveMongoRepository<Recurso, String> {
+    Flux<Recurso> findByArea(String area);
 }
